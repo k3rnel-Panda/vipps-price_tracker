c633f9096c40631b1b27f407d6c14e9c8a80ff44
k3rnel-Panda <38436760+k3rnel-Panda@users.noreply.github.com>
2018-08-07 19:58:13 +0530

    Update constants.py
    Updated the Mailgun API Key, URL and recipient address.

--- models/alerts/constants.py
+++ models/alerts/constants.py
@@ -1,7 +1,7 @@
 
-URL = "https://api.mailgun.net/v3/sandboxe5e685134a5c41a39039382edf6fec9b.mailgun.org"
-API_KEY = "3657b575ff7d4d61aa84a9f2be2559e4-a5d1a068-c7be473d"
-FROM = "postmaster@sandboxe5e685134a5c41a39039382edf6fec9b.mailgun.org"
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
 
 COLLECTION = "alerts"
 ALERT_TIMEOUT = 10
